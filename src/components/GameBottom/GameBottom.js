947d6ed32ed0ccc7eb57c17ba4ceba2ed6a125b9
RohaanPawar <rohanpawar07@gmail.com>
2021-01-17 15:43:49 +0530

    integrated design in react structure

---
+++ src/components/GameBottom/GameBottom.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class GameBottom extends Component {
+  render() {
+    return (
+      <div className="game--bottom">
+        <div className="game--setting">
+          <div className="gear--wrap">
+            <i className="ttt-gear"></i>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default GameBottom;
