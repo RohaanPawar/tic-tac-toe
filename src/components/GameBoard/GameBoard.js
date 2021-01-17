cbcf15f863b32e5ef416a668d87de4a18400ab12
RohaanPawar <rohanpawar07@gmail.com>
2021-01-17 22:20:49 +0530

    fixed ios issue

--- src/components/GameBoard/GameBoard.js
+++ src/components/GameBoard/GameBoard.js
@@ -4,7 +4,11 @@ import { Spring } from "react-spring/renderprops";
 class GameBoard extends Component {
   render() {
     return (
-      <Spring from={{ opacity: 0 }} to={{ opacity: 1 }} config={{ duration: 400}}>
+      <Spring
+        from={{ opacity: 0 }}
+        to={{ opacity: 1 }}
+        config={{ duration: 400 }}
+      >
         {(props) => (
           <div style={props} className="game--board game--box">
             <div className="game--score">
@@ -16,25 +20,15 @@ class GameBoard extends Component {
               <h4>Player 2</h4>
             </div>
             <div className="box-main">
-              <div id="box-0" className="box">
-                <i className="ttt ttt-circle"></i>
-              </div>
+              <div id="box-0" className="box"></div>
               <div id="box-1" className="box"></div>
               <div id="box-2" className="box"></div>
-              <div id="box-3" className="box">
-                <i className="ttt ttt-circle"></i>
-              </div>
-              <div id="box-4" className="box">
-                <i className="ttt ttt-x-mark"></i>
-              </div>
-              <div id="box-5" className="box">
-                <i className="ttt ttt-x-mark"></i>
-              </div>
+              <div id="box-3" className="box"></div>
+              <div id="box-4" className="box"></div>
+              <div id="box-5" className="box"></div>
               <div id="box-6" className="box"></div>
               <div id="box-7" className="box"></div>
-              <div id="box-8" className="box">
-                <i className="ttt ttt-x-mark"></i>
-              </div>
+              <div id="box-8" className="box"></div>
             </div>
           </div>
         )}
